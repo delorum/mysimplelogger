5c7d5f6641fcbee538dc6a0aaeab50cf2403f2d3
Andrey Borunov <a.borunov@netris.ru>
2015-09-14 12:16:24 +0300

    mysimplelogger v1.4: added 't:Throwable' argument to all logger methods

--- src/main/scala/com/github/dunnololda/mysimplelogger/MySimpleLogger.scala
+++ src/main/scala/com/github/dunnololda/mysimplelogger/MySimpleLogger.scala
@@ -7,10 +7,16 @@ object MySimpleLogger {
      private val log = LoggerFactory.getLogger(name)
 
      def debug(message: => String) {if (log.isDebugEnabled) log.debug(message)}
+     def debug(message: => String, t:Throwable) {if (log.isDebugEnabled) log.debug(message, t)}
+
      def info(message: => String) {if (log.isInfoEnabled) log.info(message)}
+     def info(message: => String, t:Throwable) {if (log.isInfoEnabled) log.info(message, t)}
+
      def warn(message: => String) {if (log.isWarnEnabled) log.warn(message)}
+     def warn(message: => String, t:Throwable) {if (log.isWarnEnabled) log.warn(message, t)}
+
      def error(message: => String) {if (log.isErrorEnabled) log.error(message)}
-     def error(message: => String, t:Exception) {if (log.isErrorEnabled) log.error(message, t)}
+     def error(message: => String, t:Throwable) {if (log.isErrorEnabled) log.error(message, t)}
    }
 
    def apply(name:String):MySimpleLogger = new MySimpleLogger(name)
